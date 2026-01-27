d78602cc32b58fa457d7d1fb23d89c1b4ce60fe4
DanielYang-hub <yangenyu0911@gmail.com>
2026-01-27 09:45:31 +0800

    Add initial Python scripts for basic programming concepts

---
+++ pages/class2-1.py
@@ -0,0 +1,34 @@
+# 比較運算子能同樣類型作比較
+print(1 == 1)  # True
+print(1 != 1)  # False
+print(1 > 1)  # False
+print(1 < 1)  # False
+print(1 >= 1)  # True
+print(1 <= 1)  # True
+
+
+# 邏輯運算子
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+# or 運算子,只要有一個條件為 True,結果就是 True
+print(True or True)  # True
+print(True or False)  # False
+print(False or True)  # True
+print(False or False)  # False
+
+# not 運算子
+print(not True)  # False
+print(not False)  # True
+
+# 優先順序
+# 1. () 括號
+# 2. ** 次方
+# 3. * / // % 乘 除 取商 取餘數
+# 4. + - 加減
+# 5. == != > < >= <= 比較運算子
+# 6. not
+# 7. and
+# 8. o
